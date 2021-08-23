5a29bee3b5ee7ed41dc55aecadd82fa7205c8d43
nikiv98 <89080627+nikiv98@users.noreply.github.com>
2021-08-23 14:17:25 +0300

    index page

---
+++ resources/views/publish.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ url('assets/css/bootstrap.min.css') }}">
+</head>
+<body>
+    <form class="w-25 p-3">
+        <div class="form-group">
+          <label for="exampleFormControlInput1">First Name</label>
+          <input type="text" class="form-control form-control-sm" id="exampleFormControlInput1">
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlInput1">Last Name</label>
+            <input type="text" class="form-control form-control-sm" id="exampleFormControlInput1">
+          </div>
+        <div class="form-group">
+          <label for="exampleFormControlTextarea1">Post</label>
+          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+      </form>
+</body>
+</html>
